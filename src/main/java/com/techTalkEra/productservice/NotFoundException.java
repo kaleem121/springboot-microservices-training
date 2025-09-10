acac18f5bdf3bd5efe18c9e10dd8072265e5003c
Kaleem Ahmad <kaleem.it786@gmail.com>
2025-09-11 03:55:09 +0530

    restructure the folder

--- src/main/java/com/techTalkEra/productservice/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.techTalkEra.helloservice;
-
-public class NotFoundException extends RuntimeException {
-
-	public NotFoundException(String msg) { super(msg); }
-
-}
--- src/main/java/com/techTalkEra/productservice/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.techTalkEra.helloservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
